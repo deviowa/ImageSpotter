7c73607c9cccd356c41a85493f1cf3091ce556dc
maximilian franze-soeln <deviowa@Rosss-MacBook-Air.local>
2014-07-02 20:37:52 -0500

    Added enter key search function

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ var request = require("request");
 var url = require('url') ;
 
 s.get('/search', function(req, res) {
-	console.log('It works')
+	// console.log('It works')
 	var queryObject = url.parse(req.url,true).query;
-  	console.log(queryObject);
+  	// console.log(queryObject);
 
 	request('https://api.500px.com/v1/photos/search?consumer_key=0hOeFTYiG7RifmC07gLhd8iaSbXHjEBC9vDftgX4'+'&tag=' + 
 		queryObject.q, function(error, response, body) {
   		var images = JSON.parse(body);
   		var imageArray = []
   		for (i=0; i<images.photos.length; i++) {
-  			console.log(images.photos[i].image_url);
+  			// console.log(images.photos[i].image_url);
   			imageArray.push(images.photos[i].image_url);
   		}
   		res.send(imageArray);
